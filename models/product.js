c681841e351be18bed3d515fc53da4242b5314b6
Shehara <sheharasiriwardana21@gmail.com>
2025-10-20 18:03:39 +0530

    product model

---
+++ models/product.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    productId: {
+        type : String,
+        required : true,
+        unique : true
+    },
+    name : {
+        type : String,
+        require : true
+    },
+    altName : {
+        type : [String],
+        default : []
+    },
+    labelledPrice : {
+        type : Number,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    image : {
+        type : [String],
+        default : ["/default-product.jpg"]
+    }
+})
